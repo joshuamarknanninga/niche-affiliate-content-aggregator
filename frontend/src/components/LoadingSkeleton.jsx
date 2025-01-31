968b9fea66a0d84bc767421f8eedbfb4158ce6ac
Joshua Nanninga <jmarknanninga@gmail.com>
2025-01-31 13:17:37 -0600

    added loading skeleton

---
+++ frontend/src/components/LoadingSkeleton.jsx
@@ -0,0 +1,10 @@
+// Create components/LoadingSkeleton.jsx
+export default function LoadingSkeleton() {
+    return (
+      <div className="skeleton">
+        <div className="skeleton-image"></div>
+        <div className="skeleton-text"></div>
+        <div className="skeleton-text"></div>
+      </div>
+    );
+  }
